c49d7c6844de182080c93f42caaf72f81abb2376
Bruno Cau <brunoalcau@gmail.com>
2014-06-22 10:26:31 -0300

    Create Project exemple package

---
+++ Filial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNugetExemplePackage
+{
+    public class Filial
+    {
+        public String NomeFantasia { get; set; }
+    }
+}
